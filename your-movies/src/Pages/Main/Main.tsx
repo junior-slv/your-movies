8a00b0ce8bb7a1f1bc228651034ebafd79a5f1b3
Junior <dirceusilva0422@gmail.com>
2022-11-19 02:42:32 -0300

    style changes

--- your-movies/src/Pages/Main/Main.tsx
+++ your-movies/src/Pages/Main/Main.tsx
@@ -6,31 +6,37 @@ import { api_key } from '../../api'
 type Response = {
   overview: string;
   poster_path: string;
+  original_title: string;
 };
 function App() {
   const [imagem, setImagem] = useState("");
   const [resumo, setResumo] = useState("");
+  const [titulo, setTitulo] = useState("");
+
   useEffect(() => {
     axios
      .get<Response>(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`)
      .then((res) => {
       setImagem(res.data.poster_path);
       setResumo(res.data.overview);
-      console.log('massa')
+      setTitulo(res.data.original_title);
      })
 
      .catch((err) => {
       console.log(err);
     })
   })
-  
-  
   return (
+
     <div className="container">
-      <header>
+      <div className="movie-poster">
+        <img src={`https://image.tmdb.org/t/p/w500${imagem}`}/> 
+      </div>
+      <div className="movie-menu">
         <h1>O que devo assistir a seguir?</h1>
-        <p>{}</p>
-      </header>
+        <p>{titulo}</p>
+        <p>{resumo}</p>
+      </div>
       
     </div>
   );
